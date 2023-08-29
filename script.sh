b1a8bff73b55738b3b479d93428768b94b21e577
ziaa11 <ziaprivate11@gmail.com>
2023-08-29 14:32:17 +0530

    Added my name to script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@
 # The second argument is the name of the python file that must be run 
 # Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
 # Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
-# Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
+# Once you have tested out the script and verified it's working, commit it to a branch: <zia>
